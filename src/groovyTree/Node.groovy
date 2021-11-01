4094aee07478c3585cc83dc585ae208faabe0206
Benoit Pasquereau <bpasquereau@gmail.com>
2021-11-01 18:15:11 +0000

    Simplified implementation

--- src/groovyTree/Node.groovy
+++ src/groovyTree/Node.groovy
@@ -1,7 +1,5 @@
 package groovyTree
 
-import groovy.transform.Immutable
-
 // Cannot be declared @Immutable because all fields (including value) would need be to immutable
 
 class Node<T> implements Tree<T> {
@@ -24,34 +22,34 @@ class Node<T> implements Tree<T> {
         this.value == node.value && left == node.left && right == node.right
     }
 
-        @Override
+    @Override
     Tuple2<Tree<Tuple2<T, Integer>>, Integer> addId(Integer index) {
         Tuple2<Tree<Tuple2<T,Integer>>,Integer> newLeft = left.addId(index)
         Tuple2<Tree<Tuple2<T,Integer>>,Integer> newRight = right.addId(newLeft.second)
 
-        return new Tuple2<Tree<Tuple2<T, Integer>, Integer>>(
+        return new Tuple2<>(
                 new Node(new Tuple2<T, Integer>(value, newRight.second),
                         newLeft.first,
                         newRight.first),
-                newRight.second + 1);
+                newRight.second + 1)
     }
 
     @Override
     String toString() {
-        StringBuilder sb = new StringBuilder("(");
+        StringBuilder sb = new StringBuilder("(")
         def addTo = { v ->
-            String s = v.toString();
+            String s = v.toString()
             if (s != "") {
-                sb.append(",");
-                sb.append(s);
+                sb.append(",")
+                sb.append(s)
             }
             return s
         }
 
-        sb.append(value.toString());
-        addTo(left);
-        addTo(right);
-        sb.append(")");
-        return sb.toString();
+        sb.append(value.toString())
+        addTo(left)
+        addTo(right)
+        sb.append(")")
+        return sb.toString()
     }
 }
